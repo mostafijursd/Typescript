e9f62a4561c3aeb05f041cc129ce5655feb6d005
Mostafijur Rahaman <148426873+mostafijursd@users.noreply.github.com>
2024-02-01 20:28:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+function add(a, b) {
+    return a + b;
+}
+var data = add(10, 30);
+console.log(data);
---
+++ app.ts
@@ -0,0 +1,6 @@
+ function add(a:number,b:number){
+    return a+b
+ }
+ let data=add(10,30)
+ console.log(data);
+ 
