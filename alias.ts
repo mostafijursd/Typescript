5c1b71deee83c78fcba2ed1a450734690e2c042e
Mostafijur Rahaman <148426873+mostafijursd@users.noreply.github.com>
2024-02-08 15:22:34 +0530

    change

--- alias.ts
+++
@@ -1,8 +0,0 @@
-type varType= string| number|undefined;
- let a:varType=10;
- let b:varType='hello'
- let c:varType=undefined ;
- console.log(a);
- console.log(b);
- console.log(c);
- 
---
+++ app01/alias.ts
@@ -0,0 +1,9 @@
+// type varType= string| number|undefined;
+//  let a:varType=10;
+//  let b:varType='hello'
+//  let c:varType=undefined ;
+//  console.log(a);
+//  console.log(b);
+//  console.log(c);
+ 
+console.log('this is build');
